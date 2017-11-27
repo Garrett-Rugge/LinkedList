1fdf9721b80291397068ec7547384fd0ee382b6d
Garrett <garrett.rugge@gmail.com>
2017-11-27 14:32:22 -0800

    x

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -19,7 +19,7 @@ private:
 class List { 
 public:
 	List(); 
-	~List() {}
+	~List() {} 
 	bool empty() const;
 	void push_back(int x);
 	void push_front(int x);
@@ -41,7 +41,6 @@ public:
 	int & operator*();
 private:
 	Link* position;
-	List* container;
 	friend class List;
 };
 
@@ -96,16 +95,18 @@ void List::pop_front(){ //removes the first part of the list
 	first = first->next;
 }
 void List::insert(Iterator iter, int x){ //pushes a number into a specific part of the list
-	if(iter.position == NULL) //skips un-needed steps if the list is empty
-	{
-		push_back(x);
+	assert(iter.position !=NULL);
+	if(iter.position == first) {
+		first = new Link(x);
 		return;
 	}
-	Link* after = iter.position;
-	Link* new_link = new Link(x); 
+	Link * prev = first;
+	Link * after = iter.position;
+	while(prev->next != iter.position)
+		prev = prev->next;
+	prev->next = new Link(x);
+	Link * new_link = prev->next;
 	new_link->next = after;
-	if(first == NULL)
-		first = new_link;
 }
 Iterator::Iterator(){
 	position = NULL;
@@ -144,100 +145,42 @@ int List::size(){ //return the size of the list
 	}
 	return count;
 }
-void test1(){ //tests push back and push front
-	List a;
-	assert(a.empty() == true);
-	a.push_back(29);
-	assert(a.empty() == false);
-	Iterator it;
-	it = a.begin();
-	assert(a.size() == 1);
-	a.push_front(3);
-	assert(*it = 29);
-	it = a.begin();
-	assert(*it = 3);
-	a.erase(it);
-	it = a.begin();
-	assert(*it = 29);
-}
-void test2(){ //tests erase and insert
-	List b;
-	b.push_back(20);
-	Iterator it;
-	it = b.begin();
-	b.push_front(29);
-	assert(*it = 20);
-	it = b.begin();
-	assert(*it = 29);
-	b.erase(it);
-	it = b.begin();
-	assert(*it = 20);
-	b.insert(it, 21);
-	assert(*it = 21);
-	it = b.begin();
-	assert(*it = 20);
-}
-void test3() {//tests erasing the first number of a list
-	List c;
-	c.push_back(7);
-	c.push_front(5);
-	c.push_back(9);
-	Iterator it;
-	it = c.begin();
-	c.erase(it);
-	it = c.begin();
-	assert(*it = 7);
-	++it;
-	assert(*it = 9);
-}
-void test4(){//tests erasing the middle number of a list
-	List d;
-	d.push_back(5);	
-	d.push_back(7);
-	d.push_back(9);
-	Iterator it;
-	it = d.begin();
-	++it;
-	d.erase(it);
-	it = d.begin();
-	assert(*it = 5);
-	++it;
-	assert(*it = 9);
-}
-void test5(){//tests erasing the last number of a list
-	List e;
-	e.push_back(5);	
-	e.push_back(7);
-	e.push_back(9);
-	Iterator it;
-	it = e.begin();
-	++it;
-	++it;
-	e.erase(it);
-	it = e.begin();
-	assert(*it = 5);
-	++it;
-	assert(*it = 7);
-	++it;
-}
-void test6(){ //size function test
-	List f;
-	assert(f.size() == 0);
-	f.push_back(5);
-	assert(f.size() == 1);
-	f.push_back(7);
-	assert(f.size() == 2);
-	f.push_back(9);
-	assert(f.size() == 3);	
-}
 
+void test1(){ //tests constructor, push back, push front, and empty
+	List a;
+	assert(a.empty());
+	a.push_back(5);
+	assert(a.empty() == false);
+	a.push_front(7);
+	a.push_back(9);
+}
+void test2(){ //tests iterator functions
+	List b;
+	b.push_back(5);
+	b.push_back(6);
+	b.push_back(7);
+	Iterator it;
+	it = b.begin();
+	assert(*it == 5);
+	++it;
+	assert(*it == 6);
+	++it;
+	assert(*it == 7);
+	b.insert(it, 10);
+	it = b.begin();
+	++it;
+	++it;
+	++it;	
+	b.erase(it);
+	it = b.
+		begin();
+	++it;
+	++it;
+	assert(*it == 10);
+}
 int main() { //tests
 	test1();
 	test2();
-	test3();
-	test4();
-	test5();
-	test6();
 	
 	cout << "All tests passed." << endl;
 }
